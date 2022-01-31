4bdb81654d8cf8321933ff42e08ff5d631c63ee2
21ciprian <2021ciprianoffice@gmail.com>
2022-01-31 18:54:32 +0000

    Added audience to the auth0 provider

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ ReactDOM.render(
 		<Auth0Provider
 			domain={process.env.REACT_APP_DOMAIN}
 			clientId={process.env.REACT_APP_CLIENT_ID}
+			audience={process.env.REACT_APP_AUDIENCE}
 			redirectUri={window.location.origin}>
 			<App />
 		</Auth0Provider>
