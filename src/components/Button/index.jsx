328998d642d47497569a572e973bcf6fef30af01
21ciprian <2021ciprianoffice@gmail.com>
2022-01-31 09:41:16 +0000

    Added animation to Button component

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,8 +1,13 @@
 import Button from '@mui/material/Button'
 import React from 'react'
+import css from './Button.module.css'
 function ButtonComponent({text}) {
 	return (
-		<Button variant='contained' size='xs' color='success'>
+		<Button
+			variant='contained'
+			size='xs'
+			color='success'
+			className={css.button}>
 			{text}
 		</Button>
 	)
